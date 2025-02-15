8af41549cf7443633ba65ca6d70c7480721e2b79
YuannZing <dirgafarrel2008@gmail.com>
2025-02-15 22:39:56 +0700

    p

--- berita.php
+++ berita.php
@@ -1,92 +1,35 @@
 <link href="berita.css" rel="stylesheet">
 
 <div class="banner">
-<div class="hero">
-<img style="height:300px; width:100%; object-fit: cover;" src="./media/Wireframe - 6 (2).png"></div>
-            <div class="judul">
-            <h2><B>BERITA</B></h2> 
-            <div class="garis"></div>
-            </div>
-            </div>
-            
-            <div class="berita">
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
+  <div class="hero">
+    <img style="height:300px; width:100%; object-fit: cover;" src="./media/Wireframe - 6 (2).png">
   </div>
-</div>
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-                    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
+  <div class="judul">
+    <h2><b>BERITA</b></h2>
+    <div class="garis"></div>
   </div>
 </div>
 
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-                    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
-  </div>
-</div>
+<div class="berita">
+  <?php
+  include 'etmin/koneksi.php';
 
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-                    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
-  </div>
-</div>
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-                    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
-  </div>
-</div>
-            <div class="card" style="width: 18rem;" >
-  <img src="./media/upacara kesaktian pancasila.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Upacara hari kesaktian pancasila</h5>
-    <p>Meriah dan Penuh Warna
-                    SMP Negeri 1 Wagir
-                    Sumpah Pemuda dengan
-                    Parade Baju Adat
-                    2 November 2024</p>
-                    <a href="beritaSelengkapnya.php" class="btn btn-primary">BACA SELENGKAPNYA</a>
-  </div>
-</div>
+  // Query untuk mengambil data berita
+  $sql = "SELECT id, title, content, image FROM news";
+  $result = $conn->query($sql);
 
-
-
-
+  // Loop berita
+  while ($row = $result->fetch_assoc()) : ?>
+    <div class="card" style="width: 18rem;">
+      <img src="etmin/media/<?= $row['image'] ?>" class="card-img-top" alt="Gambar Berita">
+      <div class="card-body">
+        <h5 class="card-title"><?= $row['title'] ?></h5>
+        <p><?= $row['content'] ?></p>
+        <a href="beritaSelengkapnya.php?id=<?= $row['id'] ?>" class="btn btn-primary">Baca Selengkapnya</a>
+      </div>
+    </div>
+  <?php endwhile; ?>
 
 </div>
+
+<?php $conn->close(); ?>
